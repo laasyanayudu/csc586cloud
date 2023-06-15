c33c7ecc378d59974c8f6cdefe71fe0d1da296cc
laasyanayudu <81281050+laasyanayudu@users.noreply.github.com>
2023-06-15 18:35:27 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -10,9 +10,9 @@ link = request.LAN("lan")
 # Create a XenVM
 for i in range(2):
   if i == 0:
-    node = request.XenVM("webserver")
+    node = request.XenVM("control")
   else: 
-    node = request.XenVM("observer")
+    node = request.XenVM("host")
     
   node.routable_control_ip = "true"
   node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU20-64-STD"
